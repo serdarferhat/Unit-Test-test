c22f0942db00fefa4b04b692284ad089b1f48a84
serdarferhat <cylnsrdrr@gmail.com>
2023-02-18 19:55:35 +0300

    cesit

--- src/pages/Cesit.jsx
+++ src/pages/Cesit.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import urls from './urls';
+import axios from 'axios';
+
+
 
 const Cesit = () => {
+const [cesit,setCesit]=useState([]);
+
+  useEffect(() => {
+    axios.get(urls.cesit)
+    .then((res)=>console.log(res.data))
+    .catch()
+   
+  }, []);
+
   return (
-    <div>Cesit</div>
+    <div>
+      sa
+      </div>
   )
 }
 
--- src/pages/test/Cesit.test.jsx
+++ src/pages/test/Cesit.test.jsx
@@ -1,8 +1,14 @@
 import Cesit from "../Cesit"
+import { render, screen } from "@testing-library/react";
+/*
+!get  //domda var ise yerelden 
+!guery  //domda yok ise
+!find   //async/element dom yok ise,veri çekiliyorsa
+*/
 
 
-
-
-test("Veriler ekrana basıldı mı?",()=>{
+test("Veriler ekrana basıldı mı?",async()=>{
     render(<Cesit/>)
+    const images =await screen.findAllByRole("img",{name:"cesit"})
+    expect(images).toHaveLength(4);
 })
