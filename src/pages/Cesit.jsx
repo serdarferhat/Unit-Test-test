dc2481502f7c18aef4e8429a5668d77d3993ec83
serdarferhat <cylnsrdrr@gmail.com>
2023-02-19 20:48:13 +0300

    cesit-css

--- src/pages/Cesit.jsx
+++ src/pages/Cesit.jsx
@@ -1,24 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import urls from './urls';
-import axios from 'axios';
-
-
+import React, { useEffect, useState } from "react";
+import urls from "../urls";
+import axios from "axios";
 
 const Cesit = () => {
-const [cesit,setCesit]=useState([]);
-
+  const [cesits, setCesits] = useState([]);
+  const [shop, setShop] = useState([]);
+  console.log(shop);
   useEffect(() => {
-    axios.get(urls.cesit)
-    .then((res)=>console.log(res.data))
-    .catch()
-   
+    axios
+      .get(urls.cesit)
+      .then((res) => setCesits(res.data))
+      .catch((err) => console.log(err));
   }, []);
-
+  const adet = (cesit) => {
+    const miktar = shop.filter((item) => item.name === cesit.name);
+    return miktar.length;
+  };
   return (
-    <div>
-      sa
-      </div>
-  )
-}
+    <div className="container mt-3 mb-2">
+      <h2>Çeşitler Tanesi 2 $</h2>
+      <h3>Ücret Toplam:{shop.length * 2}$</h3>
+      <div className="container d-flex justify-content-center mt-5 gap-5">
+        {cesits.map((cesit) => {
 
-export default Cesit
+     const miktar = adet(cesit)
+          return (
+            <div
+              key={cesit.name}
+              className="d-flex flex-column"
+              style={{ width: "150px" }}
+            >
+              <img src={cesit.imagePath} className="w-100" alt="" />
+              <p>{cesit.name}</p>
+              <div className="d-flex gap-2">
+                <button className="btn btn-sm btn-outline-danger">
+                  Sıfırla
+                </button>
+                <span className="lead">{miktar}</span>
+                <button
+                  className="btn btn-sm btn-outline-warning"
+                  onClick={() => setShop([...shop, cesit])}
+                >
+                  Ekle
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cesit;
---
+++ src/urls.jsx
@@ -0,0 +1,6 @@
+const urls={
+         cesit:"http://localhost:3006/cesitler",
+         sos:"http://localhost:3006/soslar"       
+}
+
+export default urls
